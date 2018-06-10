0de17c0c3d14d31a62a60e49f1ced2e665885a4c
neville <kilonzoneville@gmail.com>
2018-06-10 21:11:50 +0300

    add initial page

---
+++ locker.py
@@ -0,0 +1,13 @@
+class Account:
+    '''
+    Class generates new instances of contacts
+    '''
+
+    account_list = [] #empty list of accounts
+
+
+    def __init__(self,platform_name,account_name,account_password)
+    
+        self.platform_name = platform_name
+        self.account_name = account_name
+        self.account_password = account_password
