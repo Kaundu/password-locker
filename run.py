4255d186fa7908576774196c292c2ba270d2fbe4
neville <kilonzoneville@gmail.com>
2018-06-17 23:14:19 +0300

    Add login function

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
+def login():
+    print("Please enter username")
+    username = input('')
+    print("Please enter password")
+    password = input()
+    for line in open("origin.txt","r").readlines():
+        login_info = line.split()
+        if username == login_info[0] and password == login_info[1]:
+            print("welcome {{username}}")
+            menu()
+            return True
+
+    print("incorrect")
+
+    return false
+
 def password_gen():
     print("How long would you want your password to be?")
     length = input().int()
